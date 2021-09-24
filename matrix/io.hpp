78dbdfab3e5cd8fa81e228ff297cb8dfe8108b04
vtarnakin <66203811+vtarnakin@users.noreply.github.com>
2021-09-24 20:59:11 +0500

    fixed tabs

--- matrix/io.hpp
+++ matrix/io.hpp
@@ -10,38 +10,38 @@
 namespace tvd {
 
 template<
-	typename _Ty,
-	size_t size>
-	std::ostream & operator << ( std::ostream & o, vector<_Ty, size> v )
-	{
-	  o << "[" << size << "]{";
-	  std::ostream_iterator<_Ty> out_itr ( o, ", ");
-	  std::copy( v.begin(), v.end(), out_itr );
-	  o << "}" << std::endl;
-	  return o;
-	}
+    typename _Ty,
+    size_t size>
+    std::ostream & operator << ( std::ostream & o, vector<_Ty, size> v )
+    {
+      o << "[" << size << "]{";
+      std::ostream_iterator<_Ty> out_itr ( o, ", ");
+      std::copy( v.begin(), v.end(), out_itr );
+      o << "}" << std::endl;
+      return o;
+    }
 
 template<typename _Ty>
-	std::ostream & operator << ( std::ostream & o, matrix_view<_Ty> m )
-	{
-	  using std::endl;
-	  o << "[" << m.size()  << "]" << endl;
-	  o << "[" << m.csize() << "]" << endl;
-	  for(size_t i = 0; i < std::size( m ); i++)
-	  {
-		  o << "{ ";
+    std::ostream & operator << ( std::ostream & o, matrix_view<_Ty> m )
+    {
+      using std::endl;
+      o << "[" << m.size()  << "]" << endl;
+      o << "[" << m.csize() << "]" << endl;
+      for(size_t i = 0; i < std::size( m ); i++)
+      {
+          o << "{ ";
           std::ostream_iterator<_Ty> out_itr ( o, ", ");
-		  std::copy( m[i].begin(), m[i].end(), out_itr );
-		  o << " }" << endl;
-	  }
-	  return o;
-	}
+          std::copy( m[i].begin(), m[i].end(), out_itr );
+          o << " }" << endl;
+      }
+      return o;
+    }
 
 template<
-	typename _Ty,
-	size_t size>
-	std::ostream & operator << ( std::ostream & o, matrix<_Ty, size> m ) {
-	  return o << matrix_view<_Ty>( m );
-	}
+    typename _Ty,
+    size_t size>
+    std::ostream & operator << ( std::ostream & o, matrix<_Ty, size> m ) {
+      return o << matrix_view<_Ty>( m );
+    }
 } // tvd
 #endif
